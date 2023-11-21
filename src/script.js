52b7ab1debbc1b0a5ec176200e8a8ffe305c00e6
Jonathan <jonathan@MacBook-Pro-Catalina.local>
2023-11-20 19:07:33 -0800

    added nav

---
+++ src/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggleBtn = document.getElementById('themeToggle');
+
+    themeToggleBtn.addEventListener('click', function() {
+        const currentTheme = document.documentElement.getAttribute('data-theme');
+        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+        document.documentElement.setAttribute('data-theme', newTheme);
+    });
+});
