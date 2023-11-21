4a3b07479b7a89030b72bb06ba7609c1e614764a
Jonathan <jonathan@MacBook-Pro-Catalina.local>
2023-11-20 19:08:50 -0800

    removed any unneccesary stuff

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const themeToggleBtn = document.getElementById('themeToggle');
+const currentTheme = document.documentElement.getAttribute("data-theme");
+const newTheme = currentTheme === "light" ? "dark" : "light";
 
-    themeToggleBtn.addEventListener('click', function() {
-        const currentTheme = document.documentElement.getAttribute('data-theme');
-        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+document.documentElement.setAttribute("data-theme", newTheme);
 
-        document.documentElement.setAttribute('data-theme', newTheme);
+document.addEventListener("DOMContentLoaded", function () {
+    const themeToggleBtn = document.getElementById("themeToggle");
+
+    themeToggleBtn.addEventListener("click", function () {
+        const currentTheme =
+            document.documentElement.getAttribute("data-theme");
+        const newTheme = currentTheme === "light" ? "dark" : "light";
+
+        document.documentElement.setAttribute("data-theme", newTheme);
     });
 });
