a57fef7391674937bd7c8ada62ac0d4f00aee28b
Jonathan <jonathan@MacBook-Pro-Catalina.local>
2023-11-22 14:02:32 -0800

    added heading

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+const positions = ["Student", "Developer", "Sleeper", "Musician"];
+const positionsElement = document.getElementById("positions");
+let positionsIndex = 0;
+
+function switchPositions() {
+    setInterval(function () {
+        positionsElement.textContent = positions[positionsIndex];
+        positionsIndex = (positionsIndex + 1) % positions.length;
+    }, 3500);
+}
+
+switchPositions();
+
 const currentTheme = document.documentElement.getAttribute("data-theme");
 const newTheme = currentTheme === "light" ? "dark" : "light";
 
